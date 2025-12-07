b3d2e5e1def581d438e98f74cd8b501decdfffc3
daniel <daniel>
2025-12-07 18:53:30 +0000

    Improved plotting

---
+++ Channel-Model/link_budget/my_frft.m
@@ -0,0 +1,16 @@
+function [outputArg1,outputArg2] = my_frft(inputArg1,inputArg2)
+%MY_FRFT Summary of this function goes here
+%   Detailed explanation goes here
+arguments (Input)
+    inputArg1
+    inputArg2
+end
+
+arguments (Output)
+    outputArg1
+    outputArg2
+end
+
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
